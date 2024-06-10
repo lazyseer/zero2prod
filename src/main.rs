e2a00628b35193297106eee8238ffebbd4e98afd
lazyseer <lazyseer@gmail.com>
2024-06-11 01:02:34 +0900

    actix-web 맛보기

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}!", &name)
+}
+
+// cargo watch로 수정하면 바로바로 적용되게 해보자 (아니 이것은 마치 nodemon??)
+//   cargo watch -x check -x test -x run
+// cargo expand로 tokio::main을 확인해보자!
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
 }
